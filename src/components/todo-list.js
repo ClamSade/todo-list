18154964154343f061f5b5f37364b1ad5066bed0
ClamSade <89941944+ClamSade@users.noreply.github.com>
2022-06-26 13:39:42 +0200

    initialisation input

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 
 
-function TodoList({addtask})
+function TodoList()
 {
     const [input, setInput] = useState({task:''});
     const [array, setArray] = useState([]);
+
+    const addTask = (input) => {
+        const copy = [...array];
+        copy = [...copy, {id: array.length + 1, task: input, complete: false}];
+        setArray(copy);
+    }
+
+    copy.push({id: array.length + 1, task: input, complete: false});
 
     const handleChange = (event) => {
         const name = event.target.name;
@@ -16,8 +24,12 @@ function TodoList({addtask})
     const handleSubmit = (event) =>{
         event.preventDefault();
         array.push(input.task);
+        setInput("");
         console.log(array);
     };
+    
+    const deleteArray = (index) => () =>
+        setArray((array) => array.filter((_, i) => i !== index));
 
     return(
         <div>
@@ -35,9 +47,9 @@ function TodoList({addtask})
                 </form>
             </div>
             <div>
-                {array.map(array => {
+                {array.map((array, index) => {
                     return (
-                        <div key={array}>{array}</div>
+                        <div key={array}>{array}<button onClick={deleteArray(index)}>Delete</button></div>
                     )
                 })}
             </div>
